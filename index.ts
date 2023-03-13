3ada844daa0d332b5eb797713a442388d2f30d42
Toluwanimi <85632246+nimi-io@users.noreply.github.com>
2023-03-13 21:47:11 +0100

    Factory Example

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ interface Database<T extends BaseRecord> {
   set(newValue: T): void;
   get(id: string): T | undefined;
 }
-
+function createDatabase<T extends BaseRecord>() {
   class inMemoryDatabase<T extends BaseRecord> implements Database<T> {
     private db: Record<string, T> = {};
 
@@ -23,11 +23,15 @@ interface Database<T extends BaseRecord> {
       return this.db[id];
     }
   }
+  return inMemoryDatabase;
+}
+const pokemonDB = createDatabase<pokemon>();
+const PokemonDB = new pokemonDB();
 
-const pokemonDB = new inMemoryDatabase<pokemon>();
-pokemonDB.set({ id: "Bulbasaur", attack: 50, defense: 10 });
-pokemonDB.set({ id: "BigBulba", attack: 100, defense: 200 });
+PokemonDB.set({ id: "Bulbasaur", attack: 50, defense: 10 });
+PokemonDB.set({ id: "BigBulba", attack: 100, defense: 200 });
 
-console.log(pokemonDB.get("Bulbasaur"));
-console.log(pokemonDB.get("BigBulba"));
+
+console.log(PokemonDB.get("Bulbasaur"));
+console.log(PokemonDB.get("BigBulba"));
 console.log(pokemonDB);
