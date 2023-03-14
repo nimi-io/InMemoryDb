f1755524c4ccd0dd6bbffb45c92f17b9402f52f2
Toluwanimi <85632246+nimi-io@users.noreply.github.com>
2023-03-14 07:32:57 +0100

    strategy

--- index.ts
+++ index.ts
@@ -85,6 +85,28 @@ function createDatabase<T extends BaseRecord>() {
     visit(visitor: (item: T) => void): void {
       Object.values(this.db).forEach(visitor);
     }
+
+    ////strategy
+
+    selectBest(scoreStrategy: (item: T) => number): T | undefined {
+      const found: {
+        max: number;
+        item: T | undefined;
+      } = {
+        max: 0,
+        item: undefined,
+      };
+      Object.values(this.db).reduce((f, item) => {
+        const score = scoreStrategy(item);
+        if (score > f.max) {
+          f.max = score;
+          f.item = item;
+        }
+        return f;
+      }, found);
+
+      return found.item;
+    }
   }
   // const db = new inMemoryDatabase();
   return inMemoryDatabase;
@@ -98,8 +120,9 @@ PokemonDB.instance.set({ id: "Bulbasaur", attack: 50, defense: 10 });
 // unsubscribe();
 
 PokemonDB.instance.set({ id: "BigBulba", attack: 100, defense: 200 });
-PokemonDB.instance.visit((item: { id: any; }) => console.log(item.id));
-
+PokemonDB.instance.visit((item: { id: any }) => console.log(item.id));
+const bestDefence = PokemonDB.instance.selectBest(({ defence }) => defence);
+console.log(bestDefence?.id);
 // console.log(PokemonDB.instance.get("Bulbasaur"));
 
 // console.log(PokemonDB.instance.get("BigBulba"));
